caa140709654cf8a0e331db80ac5f3fddd2a670f
Aldair <aldair.pardo@pluriza.com>
2024-12-05 23:08:57 -0500

    chore: first commit with Express and Apollo Server

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+
+async function startServer() {
+    const app = express();
+    const server = new ApolloServer({
+        typeDefs: `
+        type Query {
+        hello: String
+        }
+    `,
+        resolvers: {
+            Query: {
+                hello: () => "Hello world!",
+            },
+        },
+    });
+
+    await server.start();
+    server.applyMiddleware({ app, path: "/graphql" });
+
+    app.listen(4000, () => {
+        console.log("Server is running on http://localhost:4000/graphql");
+    });
+}
+
+startServer();
